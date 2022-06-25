4887c131f3d79af26f87f956111b9512e0b8b64c
jieun-dev1 <jkdev1324@gmail.com>
2022-06-25 16:31:56 +0900

    Stubbying 연습문제까지.

--- src/test/java/testcode_whiteship/inflearnjavatest/study/StudyServiceTest.java
+++ src/test/java/testcode_whiteship/inflearnjavatest/study/StudyServiceTest.java
@@ -19,84 +19,15 @@ import testcode_whiteship.inflearnjavatest.member.MemberService;
 @ExtendWith(MockitoExtension.class)
 class StudyServiceTest {
 
-    //방법 1
+    @Mock
+    MemberService memberService;
 
-//    @Mock
-//    MemberService memberService;
-//
-//    @Mock
-//    StudyRepository studyRepository;
-//
-//    @Test
-//    void createStudyService() {
-//        StudyService studyService = new StudyService(memberService, studyRepository);
-//        assertNotNull(studyService);
-//    }
-
-    //방법 2
-
-    @Test
-    void createNewStudy(@Mock MemberService memberService, @Mock StudyRepository studyRepository) {
-        StudyService studyService = new StudyService(memberService, studyRepository);
-        assertNotNull(studyService);
-
-        Member member = new Member();
-        member.setId(1L);
-        member.setEmail("j@gmail1.com");
-
-        //Stubbing 방법1 - when: findById(1L)가 호출이 되면, then 방금 만든 member 객체를 Optional에 감싸져서 리턴한다.
-//        when(memberService.findById(1L)).thenReturn(Optional.of(member));
-        //Stubbing 방법 2 - any() 어떤 인자가 들어가든, 값을 리던해준다. 아무거나 주세요 - 범용적일 때 쓰임.
-        when(memberService.findById(any())).thenReturn(Optional.of(member));
-        //Stubbing 방법 3 - 값을 리턴하는 게 아니라, 예외를 던져줄 수 있다.
-//        when(memberService.findById(1L)).thenThrow(new RuntimeException());
-
-//        Optional<Member> findById = memberService.findById(1L);
-
-        assertEquals("j@gmail1.com", memberService.findById(1L).get().getEmail());
-        assertEquals("j@gmail1.com", memberService.findById(2L).get().getEmail());
-
-        doThrow(new IllegalArgumentException()).when(memberService).validate(1L);
-
-        assertThrows(IllegalArgumentException.class, () -> {
-            memberService.validate(1L);
-        });
-
-        memberService.validate(2L);
-    }
-
-    @Test
-    @DisplayName("연속적인 테스트하기")
-    void createNewStudyV2(@Mock MemberService memberService,
-            @Mock StudyRepository studyRepository) {
-        StudyService studyService = new StudyService(memberService, studyRepository);
-        assertNotNull(studyService);
-
-        Member member = new Member();
-        member.setId(1L);
-        member.setEmail("j@gmail1.com");
-
-        when(memberService.findById(any()))
-                .thenReturn(Optional.of(member))
-                .thenThrow(new RuntimeException())
-                .thenReturn(Optional.empty());
-
-        Optional<Member> byId = memberService.findById(1L);
-
-        //첫 번째 호출
-        assertEquals("j@gmail1.com", byId.get().getEmail());
-        //두 번째 호출
-        assertThrows(RuntimeException.class, () -> {
-            memberService.findById(1L);
-        });
-        //세 번째 호출
-        assertEquals(Optional.empty(), memberService.findById(3L));
-
-    }
+    @Mock
+    StudyRepository studyRepository;
 
     @Test
     @DisplayName("Stubbing 연습 문제")
-    void practice(@Mock MemberService memberService, @Mock StudyRepository studyRepository) {
+    void createNewStudy() {
         StudyService studyService = new StudyService(memberService, studyRepository);
         assertNotNull(studyService);
 
