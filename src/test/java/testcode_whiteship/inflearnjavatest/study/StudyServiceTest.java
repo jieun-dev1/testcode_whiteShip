f543b9fa21744110b2bfed96276dfac0d0c331ff
jieun-dev1 <jkdev1324@gmail.com>
2022-06-25 17:20:59 +0900

    Mockito BDD 스타일 API

--- src/test/java/testcode_whiteship/inflearnjavatest/study/StudyServiceTest.java
+++ src/test/java/testcode_whiteship/inflearnjavatest/study/StudyServiceTest.java
@@ -2,7 +2,14 @@ package testcode_whiteship.inflearnjavatest.study;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.BDDMockito.then;
 import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.inOrder;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
 import static org.mockito.Mockito.when;
 
 import java.util.Optional;
@@ -10,6 +17,7 @@ import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InOrder;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import testcode_whiteship.inflearnjavatest.domain.Member;
@@ -28,6 +36,7 @@ class StudyServiceTest {
     @Test
     @DisplayName("Stubbing 연습 문제")
     void createNewStudy() {
+        //Given
         StudyService studyService = new StudyService(memberService, studyRepository);
         assertNotNull(studyService);
 
@@ -37,12 +46,22 @@ class StudyServiceTest {
 
         Study study = new Study(10, "테스트");
 
-        when(memberService.findById(1L)).thenReturn(Optional.of(member));
-        when(studyRepository.save(study)).thenReturn(study);
+//        when(memberService.findById(1L)).thenReturn(Optional.of(member));
+//        when(studyRepository.save(study)).thenReturn(study);
 
+        //when 이 given/when/then 의 when behavior 와 헷갈릴 수 있으니 given으로 바꾸어준다.
+
+        given(memberService.findById(1L)).willReturn(Optional.of(member));
+        given(studyRepository.save(study)).willReturn(study);
+
+        //When
         studyService.createNewStudy(1L, study);
+
+        //Then
         assertEquals(member, study.getOwner());
-
+        //호출 여부를 확인하는 것. notify가 study라는 매개변수를 가지고 1번 호출되었어야 한다. Verify 쓰면, n번 호출되는지 확인 가능.
+        //만약 service에 이런 메서드가 없다면, wanted but not invoked 라는 에러가 난다.
+        then(memberService).should(times(1)).notify(study);
+        then(memberService).shouldHaveNoMoreInteractions();
     }
-
 }
